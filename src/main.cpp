5f5e62a5d1135dc6285e6c65e007f245b3349c45
Arl44 <117769424+ArlTrt@users.noreply.github.com>
2025-01-23 15:01:10 +0100

    first triangle

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,24 @@ int main()
     gl::init("TPs de Rendering"); // On crée une fenêtre et on choisit son nom
     gl::maximize_window(); // On peut la maximiser si on veut
 
+    auto const triangle_mesh = gl::Mesh{{
+        .vertex_buffers = {{
+            .layout = {gl::VertexAttribute::Position2D{0 /*Index de l'attribut dans le shader, on en reparle juste après*/}},
+            .data   = {
+                -0.7f, -0.8f, // Position2D du 1er sommet
+                +0.9f, -0.7f, // Position2D du 2ème sommet
+                 0.4f, +0.9f  // Position2D du 3ème sommet
+            },
+        }},
+    }};
+
     while (gl::window_is_open())
     {
         // Rendu à chaque frame
+        glClearColor(0.6f, 0.2f, 0.5f, 1.f); // Choisis la couleur à utiliser. Les paramètres sont R, G, B, A avec des valeurs qui vont de 0 à 1
+        glClear(GL_COLOR_BUFFER_BIT); // Exécute concrètement l'action d'appliquer sur tout l'écran la couleur choisie au-dessus
+        
+        gl::bind_default_shader(); // On a besoin qu'un shader soit bind (i.e. "actif") avant de draw(). On en reparle dans la section d'après.
+        triangle_mesh.draw(); // C'est ce qu'on appelle un "draw call" : on envoie l'instruction à la carte graphique de dessiner notre mesh.
     }
 }
