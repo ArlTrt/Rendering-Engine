0eb74fe581179377d6ec8baabdcc28f93b8308ae
Arl44 <117769424+ArlTrt@users.noreply.github.com>
2025-02-21 15:21:22 +0100

    matrix

--- res/vertex.glsl
+++ res/vertex.glsl
@@ -3,6 +3,7 @@
 layout(location = 0) in vec2 in_position;
 uniform float aspect_ratio;
 uniform float time;
+uniform mat4 view_projection_matrix;
 
 void main()
 {
@@ -14,4 +15,5 @@ void main()
     position.x += 0.1 * time;
     position.y += 0.1 * time;
     gl_Position = vec4(position, 0., 1.);
+    gl_Position = view_projection_matrix * vec4(in_position, 0., 1.);
 }
