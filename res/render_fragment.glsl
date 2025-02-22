3a76f3e24dfdd955f4e8cc49af167be9a64b9203
Arl44 <117769424+ArlTrt@users.noreply.github.com>
2025-02-22 20:42:36 +0100

    post process

--- res/render_fragment.glsl
+++ res/render_fragment.glsl
@@ -5,6 +5,7 @@ out vec4 out_render_color;
 in vec3 vertex_position;
 in vec2 uv;
 uniform sampler2D render_texture;
+uniform vec4 overlay_color;
 
 void main()
 {
@@ -12,6 +13,12 @@ void main()
     //out_color = vec4(vertex_position, 1.);
     //out_color = vec4(uv.x, uv.y, 0., 1.);
 
+    
     vec4 render_color = texture(render_texture, uv);
-    out_render_color = render_color;
+    //overlay_color = vec4(0.2, 0.8, 0.4, 0.1);
+    float grayscale = dot(render_color.rgb, vec3(0.299, 0.587, 0.114));
+    //out_render_color = vec4(vec3(grayscale), render_color.a);
+    out_render_color = vec4(render_color.r, 0.0, 0.0, render_color.a);
 }
+
+    
